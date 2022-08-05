5b7c989b2bef213c24dceab52da65733951e32ab
David Smith <davidrsmith04@gmail.com>
2022-08-05 16:04:01 +0100

    fixed div grid bug/logic working

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 "use strict";
 const mainDiv = document.getElementById("container");
 const btnPrompt = document.querySelector(".gridSize");
+const allNewDivs = document.querySelector(".newDivs");
 //INLINE BLOCK METHOD
 
 // for (let i = 0; i < 16; i++) {
@@ -17,26 +18,48 @@ const btnPrompt = document.querySelector(".gridSize");
 //   mainDiv.appendChild(jump); //
 // }
 
-btnPrompt.addEventListener("click", function () {
-  let sizePrompt = prompt("How large would you like the grid?");
-  let gridSize = sizePrompt * sizePrompt;
+function removeAllChildNodes(parent) {
+  // mainDiv
+  while (parent.firstChild) {
+    // While mainDiv has firstChild
+    parent.removeChild(parent.firstChild); // mainDiv.removeChild (mainDiv.firstChild)
+  } // Because it is a while-loop, it will keep removing the firstChild until none left
+}
 
-  if (typeof sizePrompt !== "number") {
+let sizePrompt;
+let gridSize;
+let div;
+
+let init = function () {
+  sizePrompt = 0;
+  gridSize = 0;
+};
+
+btnPrompt.addEventListener("click", function () {
+  init();
+  removeAllChildNodes(mainDiv);
+  sizePrompt = prompt("How large would you like the grid?");
+  gridSize = sizePrompt * sizePrompt;
+  console.log(sizePrompt);
+  console.log(gridSize);
+
+  if (isNaN(sizePrompt)) {
     alert("Error, need a number!");
   } else if (sizePrompt > 50) {
     sizePrompt = 50;
     alert("Grid size must be 50 or less");
   }
   // SAME LOOP AND CODE BUT FOR GRID
-  else if (typeof sizePrompt === 'number') {
+  else
     for (let i = 0; i < `${gridSize}`; i++) {
-      let div = document.createElement("div");
+      div = document.createElement("div");
 
       div.style.width = "50px";
       div.style.height = "50px";
       div.classList.add("newDivs");
       // div.style.backgroundColor = "red";
       mainDiv.style.cssText += `grid-template-columns: repeat(${sizePrompt}, 50px [col-start])`;
+      console.log(sizePrompt);
       mainDiv.appendChild(div);
     }
 
@@ -65,4 +88,3 @@ btnPrompt.addEventListener("click", function () {
     });
   }
 });
-}
